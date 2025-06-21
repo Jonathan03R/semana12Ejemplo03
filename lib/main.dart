9e4b5b67dcc5009012fc8c152f7c078e6e237440
Jonathan03R <jonathanjesusroquegonzales@gmail.com>
2025-06-20 20:33:11 -0500

    seconds grafic

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    List<_PieData> pieData = <_PieData>[
+      _PieData('John', 35, 'John'),
+      _PieData('Jane', 28, 'Jane'),
+      _PieData('Alex', 34, 'Alex'),
+      _PieData('Emma', 32, 'Emma'),
+      _PieData('Mike', 40, 'Mike'),
+    ];
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
@@ -23,26 +30,54 @@ class MyApp extends StatelessWidget {
               border: Border.all(color: Colors.black, width: 1.0),
             ),
             padding: const EdgeInsets.all(18.0),
-            width: 600,
-            height: 600,
-            child: SfCartesianChart(
-              // Initialize category axis
-              primaryXAxis: CategoryAxis(),
+            width: double.infinity,
+            height: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  SfCartesianChart(
+                    title: ChartTitle(text: 'Progreso 01'),
+                    legend: Legend(
+                      isVisible: true,
+                      position: LegendPosition.bottom,
+                    ),
+                    // Initialize category axis
+                    primaryXAxis: CategoryAxis(),
 
-              series: <LineSeries<SalesData, String>>[
-                LineSeries<SalesData, String>(
-                  // Bind data source
-                  dataSource: <SalesData>[
-                    SalesData('Jan', 35),
-                    SalesData('Feb', 28),
-                    SalesData('Mar', 34),
-                    SalesData('Apr', 32),
-                    SalesData('May', 40),
-                  ],
-                  xValueMapper: (SalesData sales, _) => sales.year,
-                  yValueMapper: (SalesData sales, _) => sales.sales,
-                ),
-              ],
+                    series: <LineSeries<SalesData, String>>[
+                      LineSeries<SalesData, String>(
+                        // Bind data source
+                        dataSource: <SalesData>[
+                          SalesData('Jan', 35),
+                          SalesData('Feb', 28),
+                          SalesData('Mar', 34),
+                          SalesData('Apr', 32),
+                          SalesData('May', 40),
+                        ],
+                        xValueMapper: (SalesData sales, _) => sales.year,
+                        yValueMapper: (SalesData sales, _) => sales.sales,
+                        dataLabelSettings: DataLabelSettings(isVisible: true),
+                      ),
+                    ],
+                  ),
+                  SfCircularChart(
+                    title: ChartTitle(text: 'Sales by sales person'),
+                    legend: Legend(isVisible: true),
+                    series: <PieSeries<_PieData, String>>[
+                      PieSeries<_PieData, String>(
+                        explode: true,
+                        explodeIndex: 0,
+                        dataSource: pieData,
+                        xValueMapper: (_PieData data, _) => data.xData,
+                        yValueMapper: (_PieData data, _) => data.yData,
+                        dataLabelMapper: (_PieData data, _) => data.text,
+                        dataLabelSettings: DataLabelSettings(isVisible: true),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -55,4 +90,11 @@ class SalesData {
   SalesData(this.year, this.sales);
   final String year;
   final double sales;
+}
+
+class _PieData {
+ _PieData(this.xData, this.yData, [this.text]);
+ final String xData;
+ final num yData;
+ String? text;
 }
